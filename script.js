97eaa73964f4d67012f9905ab4413fcc9896e7a4
mathpalvipin <53302061+mathpalvipin@users.noreply.github.com>
2020-12-23 20:51:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,15 +4,18 @@ let i = 1
 setInterval(() => {
 	i++
 	const icon = document.querySelector('.section-1-icons .change')
-	icon.classList.remove('.change')
-
+	
+	icon.classList.remove('change')
 	if (i > icons.length) {
-		icons[0].classList.add('.change')
+
+icons[0].classList.add('change')
+
 		i = 1
 	} else {
+	
 		icon.nextElementSibling.classList.add('change')
 	}
-}, 4000)
+}, 1000)
 
  document.querySelector('.menu').addEventListener
 ('click',()=>{
